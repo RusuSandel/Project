132b7de0c0a12fe95b5f20a04651f139e58aa46e
belletz-28 <matteob252@gmail.com>
2020-02-21 12:31:02 +0100

    Controller Prestiti PHP

---
+++ src/WebAPI/Prestiti/autocomplete.php
@@ -0,0 +1,19 @@
+
+<?php
+
+include '../Common/connection.php';
+$partName= $_GET["text"];
+
+
+
+    $query = "SELECT Id AS value, CONCAT(Cognome,' ',Nome) AS text FROM Autori WHERE CONCAT(Cognome,' ',Nome) LIKE :part LIMIT 50";
+    $stmt = $conn->prepare($query);
+
+
+
+    $stmt->execute(['part' => "%".$partName."%"]);
+    $element = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($element);
+
+
+?>
