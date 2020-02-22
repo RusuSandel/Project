654d5e251a3a3fda6e8e08580f8fa96862e6e4ea
belletz-28 <matteob252@gmail.com>
2020-02-22 16:54:14 +0100

    Controller Prestiti Admin da testare Autocomplete.php: //da modificare per ricerca prestiti con nome utente

--- src/WebAPI/Prestiti/autocomplete.php
+++ src/WebAPI/Prestiti/autocomplete.php
@@ -3,14 +3,9 @@
 
 include '../Common/connection.php';
 $partName= $_GET["text"];
-
-
-
-    $query = "SELECT Id AS value, CONCAT(Cognome,' ',Nome) AS text FROM Autori WHERE CONCAT(Cognome,' ',Nome) LIKE :part LIMIT 50";
+    //da modificare per ricerca prestiti con nome utente
+    $query = "SELECT Id AS value, CONCAT(Cognome,' ',Nome) AS text FROM Utenti WHERE CONCAT(Cognome,' ',Nome) LIKE :part LIMIT 50";
     $stmt = $conn->prepare($query);
-
-
-
     $stmt->execute(['part' => "%".$partName."%"]);
     $element = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($element);
