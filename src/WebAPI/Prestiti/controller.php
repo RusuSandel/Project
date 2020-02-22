886ec371de9963f005caea96b65927e5bc2775eb
belletz-28 <matteob252@gmail.com>
2020-02-22 16:41:44 +0100

    Controller Prestiti Admin da testare --todo: parte js di invio e recezione dati

--- src/WebAPI/Prestiti/controller.php
+++ src/WebAPI/Prestiti/controller.php
@@ -8,7 +8,7 @@ $body= file_get_contents('php://input');
 
 switch ($method) {
     case "GET":
-       Read($conn);
+        Read($conn);
         break;
     case "POST":
         Update($body,$conn);
@@ -24,39 +24,42 @@ switch ($method) {
         break;
 }
 
-
-function Create($jsonGenere, $connector)
+function Create($jsonPrestito, $connector)
 {
-    $decode = json_decode($jsonGenere);
-    $genere = new genere($decode->Id);
-    $query ="INSERT INTO Generi (Id) VALUE (:id)";
+    $decode = json_decode($jsonPrestito);
+    $prestito = new Prestiti(
+                            $decode["IdUtente"]->IdUtente,
+                            $decode["IdLibro"]->IdLibro,
+                            $decode["DataInizioPrestito"]->DataInizioPrestito,
+                            $decode["DataFinePrestito"]->DataFinePrestito);
+    
+    $query ="INSERT INTO libriutenti (IdUtente,IdLibro,DataInizioPrestito,DataFinePrestito) 
+             VALUE (:IdUtente,:IdLibro,:DataInizioPrestito,:DataFinePrestito)";
     $stmt = $connector->prepare($query);
-    $stmt->bindParam(':id',$genere->Id,PDO::PARAM_STR);
+    $stmt->bindParam(':IdUtente',$prestito->IdUtente,PDO::PARAM_STR);
+    $stmt->bindParam(':IdLibro',$prestito->IdLibro,PDO::PARAM_STR);
+    $stmt->bindParam(':DataInizioPrestito',$prestito->DataInizioPrestito,PDO::PARAM_STR);
+    $stmt->bindParam(':DataFinePrestito',$prestito->DataFinePrestito,PDO::PARAM_STR);
     if($stmt->execute()){
-        $returnIdquery ="SELECT Id from Generi WHERE Id=:id LIMIT 1";
+        $returnIdquery ="SELECT IdUtente from libriutenti WHERE IdUtente=:IdUtente LIMIT 1";
         $stmt = $connector->prepare($returnIdquery);
-
-        $stmt->bindParam(':id',$genere->Id,PDO::PARAM_STR);
-
+        $stmt->bindParam(':IdUtente',$prestito->IdUtente,PDO::PARAM_STR);
         $stmt->execute();
         $element = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
         http_response_code(201);
         echo json_encode($element);
         return true;
     }
-
     http_response_code(503);
-    echo json_encode(array("message" => "Impossibile creare un genere."));
+    echo json_encode(array("message" => "Impossibile creare un prestito."));
     return false;
-
 }
 
 function Read($connector)
 {
     $query ="SELECT * FROM libriutenti ";
     $stmt = $connector->prepare($query);
-    if($stmt->execute($query)){
+    if($stmt->execute()){
         $element = $stmt->fetchAll(PDO::FETCH_ASSOC);
         http_response_code(200);
         echo json_encode($element);
@@ -64,71 +67,54 @@ function Read($connector)
     }
     http_response_code(404);
     echo json_encode(
-        array("message" => "No genere trovato.")
+        array("message" => "Nessun prestito nello storico")
     );
     return false;
 
 
 }
 
-function Update($jsonGenere, $connector)
+function Update($jsonPrestiti, $connector)
 {
-    $decode = json_decode($jsonGenere);
-
-
-    $genere = new genere($decode->Id);
-    $query ="UPDATE Generi SET Id=:id WHERE Id=:id";
-
+    $decode = json_decode($jsonPrestiti);
+    $prestito = new Prestiti(
+        $decode["IdUtente"]->IdUtente,
+        $decode["IdLibro"]->IdLibro,
+        $decode["DataInizioPrestito"]->DataInizioPrestito,
+        $decode["DataFinePrestito"]->DataFinePrestito);
+    $query ="UPDATE libriutenti SET IdUtente=:id WHERE IdUtente=:id";
     $stmt = $connector->prepare($query);
-
-    $stmt->bindParam(':id',$genere->Id,PDO::PARAM_INT);
-
-
-
-
+    $stmt->bindParam(':id',$prestito->IdUtente,PDO::PARAM_INT);
 
     if($stmt->execute()){
-        $returnIdquery ="SELECT Id from Generi WHERE Id=:id LIMIT 1";
+        $returnIdquery ="SELECT IdLibro from libriutenti WHERE IdLibro=:id LIMIT 1";
         $stmt = $connector->prepare($returnIdquery);
-
-        $stmt->bindParam(':nome',$genere->Id,PDO::PARAM_STR);
-
+        $stmt->bindParam(':id',$prestito->IdLibro,PDO::PARAM_STR);
         $stmt->execute();
         $element = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
         http_response_code(200);
         echo json_encode($element);
         return true;
     }
 
     http_response_code(503);
-    echo json_encode(array("message" => "Impossibile aggiornare un genere."));
+    echo json_encode(array("message" => "Impossibile aggiornare il prestito."));
     return false;
-
 }
 
 function Delete($id , $connector)
 {
-    $query ="DELETE FROM Generi WHERE Id=:id";
-
+    $query ="DELETE FROM libriutenti WHERE IdUtente=:id";
     $stmt = $connector->prepare($query);
-
     $stmt->bindParam(':id',$id);
-
-
-
-
     if($stmt->execute()){
-
-
         http_response_code(200);
         echo $id;
         return true;
     }
-
+    
     http_response_code(503);
-    echo json_encode(array("message" => "Impossibile cancellare un genere."));
+    echo json_encode(array("message" => "Impossibile cancellare il prestito."));
     return false;
 }
 
-?>
