51e645a6f9725c2d665dded79d8edc68f275eb1d
Belletz-28 <matteob252@gmail.com>
2020-03-15 12:59:32 +0100

    Creazione Bind: -Riduzione righe di codice -Ultimazione DB

--- src/WebAPI/Prestiti/controller.php
+++ src/WebAPI/Prestiti/controller.php
@@ -24,6 +24,24 @@ switch ($method) {
         break;
 }
 
+function Bind($connector,$query,$prestito)
+{
+    $stmt = $connector->prepare($query);
+    $stmt->bindParam(':IdUtente',$prestito->IdUtente,PDO::PARAM_STR);
+    $stmt->bindParam(':IdLibro',$prestito->IdLibro,PDO::PARAM_STR);
+    $stmt->bindParam(':DataInizioPrestito',$prestito->DataInizioPrestito,PDO::PARAM_STR);
+    $stmt->bindParam(':DataFinePrestito',$prestito->DataFinePrestito,PDO::PARAM_STR);
+    $stmt = $connector->prepare($query);
+    if($stmt->execute()){
+        $returnIdquery ="SELECT IdUtente from LibriUtenti WHERE IdUtente=:IdUtente LIMIT 1";
+        $stmt = $connector->prepare($returnIdquery);
+        $stmt->bindParam(':IdUtente',$prestito->IdUtente,PDO::PARAM_STR);
+        $stmt->execute();
+        $element = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($element);
+        return true;
+    }
+}
 function Create($jsonPrestito, $connector)
 {
     $decode = json_decode($jsonPrestito);
@@ -35,19 +53,13 @@ function Create($jsonPrestito, $connector)
     
     $query ="INSERT INTO LibriUtenti (IdUtente,IdLibro,DataInizioPrestito,DataFinePrestito) 
              VALUE (:IdUtente,:IdLibro,:DataInizioPrestito,:DataFinePrestito)";
-    $stmt = $connector->prepare($query);
-    $stmt->bindParam(':IdUtente',$prestito->IdUtente,PDO::PARAM_STR);
-    $stmt->bindParam(':IdLibro',$prestito->IdLibro,PDO::PARAM_STR);
-    $stmt->bindParam(':DataInizioPrestito',$prestito->DataInizioPrestito,PDO::PARAM_STR);
-    $stmt->bindParam(':DataFinePrestito',$prestito->DataFinePrestito,PDO::PARAM_STR);
-    if($stmt->execute()){
-        $returnIdquery ="SELECT IdUtente from LibriUtenti WHERE IdUtente=:IdUtente LIMIT 1";
-        $stmt = $connector->prepare($returnIdquery);
-        $stmt->bindParam(':IdUtente',$prestito->IdUtente,PDO::PARAM_STR);
-        $stmt->execute();
-        $element = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        http_response_code(201);
-        echo json_encode($element);
+
+    $queryLibri ="UPDATE Libri SET IdUtentePrestito=:IdUtente,Stato='N', DataInizioPrestito=:DataInizioPrestito, 
+                  DataFinePrestitoPrevista=:DataFinePrestito WHERE Id=:id";
+    $resultQuery = Bind($connector,$query,$prestito);
+    $resultQueryLibri = Bind($connector,$queryLibri,$prestito);
+    if($resultQuery  == true && $resultQueryLibri == true) {
+        http_response_code(200);
         return true;
     }
     http_response_code(503);
@@ -82,22 +94,12 @@ function Update($jsonPrestiti, $connector)
         $decode["IdLibro"]->IdLibro,
         $decode["DataInizioPrestito"]->DataInizioPrestito,
         $decode["DataFinePrestito"]->DataFinePrestito);
-    $query ="UPDATE LibriUtenti SET IdUtente=:id WHERE IdUtente=:id";
-    $stmt = $connector->prepare($query);
-    $stmt->bindParam(':id',$prestito->IdUtente,PDO::PARAM_INT);
-
-    if($stmt->execute()){
-        $returnIdquery ="SELECT IdLibro from LibriUtenti WHERE IdLibro=:id LIMIT 1";
-        $stmt = $connector->prepare($returnIdquery);
-        $stmt->bindParam(':id',$prestito->IdLibro,PDO::PARAM_STR);
-        $stmt->execute();
-        $element = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $query ="UPDATE LibriUtenti SET IdUtente=:id,IdLibro=:IdLibro,DataInizioPrestito=:DataInizioPrestito,DataFinePrestito=:DataFinePrestito WHERE IdUtente=:id";
+    $resultQuery = Bind($connector,$query,$prestito);
+    if($resultQuery  == true) {
         http_response_code(200);
-        echo json_encode($element);
         return true;
-    }
-
-    http_response_code(503);
+    }    http_response_code(503);
     echo json_encode(array("message" => "Impossibile aggiornare il prestito."));
     return false;
 }
